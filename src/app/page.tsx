eca0a4524f2b447ab63f2cb5318be7029fa03393
Jordan Pillai <pillaijordan@gmail.com>
2024-01-10 14:57:12 +0000

    add snippets detail page and link to edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 rounded border"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">
+        Sorry, but we couldn&apos;t find that particular snippet
+      </h1>
+    </div>
+  );
+}
