603b86905db44a0949cfc8561df445ac8ed7df79
Jordan Pillai <pillaijordan@gmail.com>
2024-01-10 14:11:29 +0000

    Boiler plate setup

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,53 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
+          />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
